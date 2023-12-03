38cfb458460959572a4b01f63c74de487606de46
dharshini-ah <152751653+dharshini-ah@users.noreply.github.com>
2023-12-03 06:49:03 +0530

    Update sl1_app.py

--- sl1_app.py
+++ sl1_app.py
@@ -4,7 +4,7 @@ import yfinance as yf
 
 st.write('New app demo')
 
-max_date = '2023-12-01'
+max_date = '2023-11-01'
 new_data = yf.download('AAPL', start=pd.to_datetime(max_date))
 
 st.write(new_data)
