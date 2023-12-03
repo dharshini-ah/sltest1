f55b2a5d891643aa4ea3c6d8669da6fa9ef6d2a8
dharshini-ah <152751653+dharshini-ah@users.noreply.github.com>
2023-12-03 06:57:34 +0530

    Update sl1_app.py

--- sl1_app.py
+++ sl1_app.py
@@ -11,8 +11,19 @@ df = yf.download('AAPL', start=pd.to_datetime(max_date))
 
 st.write(df)
 
-csv = df.to_csv().encode()
-b64 = base64.b64encode(csv).decode()
-href = f'Download CSV File'
-st.markdown(href, unsafe_allow_html=True)
+#csv = df.to_csv().encode()
+#b64 = base64.b64encode(csv).decode()
+#href = f'Download CSV File'
+#st.markdown(href, unsafe_allow_html=True)
+
+
+def get_table_download_link_csv(df):
+    #csv = df.to_csv(index=False)
+    csv = df.to_csv().encode()
+    #b64 = base64.b64encode(csv.encode()).decode() 
+    b64 = base64.b64encode(csv).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="captura.csv" target="_blank">Download csv file</a>'
+    return href
+
+st.markdown(get_table_download_link_csv(df), unsafe_allow_html=True)
 
