a3c25efdeaf9305f4bcfcc89094293fbb12e0dca
dharshini-ah <152751653+dharshini-ah@users.noreply.github.com>
2023-12-03 06:53:25 +0530

    Update sl1_app.py

--- sl1_app.py
+++ sl1_app.py
@@ -9,3 +9,8 @@ new_data = yf.download('AAPL', start=pd.to_datetime(max_date))
 
 st.write(new_data)
 
+csv = df.to_csv().encode()
+b64 = base64.b64encode(csv).decode()
+href = f’Download CSV File’
+st.markdown(href, unsafe_allow_html=True)
+
